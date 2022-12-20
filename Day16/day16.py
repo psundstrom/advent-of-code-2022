47fd97063ec8be53a3403909cedcd8243b51cb3f
Peter Sundström <peter.sundstrom@frotorp.se>
2022-12-20 18:41:54 +0100

    Day16 part 2

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,5 +1,6 @@
 from collections import deque
 import networkx as nx
+from itertools import combinations
 
 
 with open('Day16/input.txt') as file:
@@ -19,9 +20,6 @@ for line in lines:
     for tunnel in tunnels:
         G.add_edge(valve,tunnel)
     opened_.append(flowrate==0)
-
-for key in V.keys():
-    print(key,V[key][0])
 
 def find_shortest_path(V,source,target):
     return nx.shortest_path_length(G,source,target)
@@ -48,15 +46,21 @@ def find_shortest_path(V,source,target):
 opened = [k for i,k in enumerate(V.keys()) if opened_[i]]
 unopened = [k for i,k in enumerate(V.keys()) if not opened_[i]]
 
-print(opened)
-print(unopened)
-print(len(unopened))
-
+shortestpaths={}
+for valve1 in unopened+['AA']:
+    shortestpaths[valve1]={}
+    for valve2 in unopened:
+        shortestpaths[valve1][valve2]=find_shortest_path(V,valve1,valve2)
+print('paths found')
 n = 0
-def get_pressure(start,pressure,minutes,unopened,opened=[]):
+def get_pressure(start,pressure,minutes,unopened,oldmax=0,opened=[]):
     global n
     n+=1
-    if minutes<=0:
+    if pressure+sum([V[valve][0]*(minutes-shortestpaths[start][valve]) for valve in unopened])<oldmax:
+        # print('prune')
+        return 0
+    if minutes<=min([shortestpaths[start][valve] for valve in unopened]):
+        # print('prune2')
         return pressure
     else:
         if start in unopened:
@@ -66,11 +70,32 @@ def get_pressure(start,pressure,minutes,unopened,opened=[]):
             pressure+=V[start][0]*minutes
         if len(unopened)==0:
             return pressure
-        return max([get_pressure(valve,pressure,minutes-find_shortest_path(V,start,valve),unopened.copy(),opened.copy()) for valve in unopened])
+
+        pr=[]
+        for valve in unopened:
+            if len(pr)>0:
+                maxpr=max(pr)
+            else:
+                maxpr=0
+            pr.append(get_pressure(valve,pressure,minutes-shortestpaths[start][valve],unopened.copy(),maxpr,opened.copy()))
+        return max(pr)
 
 uo = unopened.copy()
-print(get_pressure('AA',0,30,uo))
-print(n)
+max1 = get_pressure('AA',0,30,uo)
+# max1=1896
+print(max1)
+# print(n)
 
-print([(find_shortest_path(V,'AA',v)) for v in V.keys()])
-print([nx.shortest_path_length(G,'AA',v) for v in V.keys()])
+max2=max1
+for i in range(1,len(unopened)):
+    print(i)
+    for valves in combinations(unopened,i):
+        unopened_ = unopened.copy()
+        for item in valves:
+            unopened_.remove(item)
+        v1 = get_pressure('AA',0,26,unopened_)
+        v2 = get_pressure('AA',0,26,list(valves),max2-v1)
+        if v1+v2>max2:
+            max2=v1+v2
+
+print(max2)
