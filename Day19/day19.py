5c9dbf287fe7da4a8bcf6582bdcd9829e87ba6bb
Peter Sundström <peter.sundstrom@modelon.com>
2022-12-25 20:10:09 +0000

    Recursion, probably needs more pruning

--- Day19/day19.py
+++ Day19/day19.py
@@ -2,13 +2,10 @@
 # claycost=2
 # obsidiancost=(3,14)
 # geodecost=(2,7)
+from time import sleep
+from copy import deepcopy
 
-orecost=2
-claycost=3
-obsidiancost=(3,8)
-geodecost=(3,12)
-
-costs=[(2,0,0,0),(3,0,0,0),(0,3,8,0),(0,3,0,12)]
+costs=[(2,0,0,0),(3,0,0,0),(3,8,0,0),(3,0,12,0)]
 
 robots = [1,0,0,0]
 
@@ -17,29 +14,47 @@ resources=[0,0,0,0]
 minutes=0
 
 def get_purchases(resources,costs):
-
     return [min([r//c for r,c in zip(resources,cost) if c>0]) for cost in costs]
 
-def get_geodes(robots,resources,costs,minutes):
-    if minutes==24:
-        return resources,resources__
-    while True:
-        p = get_purchases(resources,costs)
-        print(p)
-        minutes+=1
-        resources=[rc+r for rc,r in zip(resources,robots)]
-        for i,item in enumerate(p):
-            resources__ = [0,0,0,0]
-            if p[i]>0:
-                newrobots=[r+rp for r,rp in zip(robots,[1 if j==i else 0 for j in range(0,4)])]
-                newresources=[r-c for r,c in zip(resources,costs[i])]
-                resources_ = get_geodes(newrobots,newresources,costs,minutes)
-                if resources_[3]>resources__[3]:
-                    resources__=resources_
-        resources_ = get_geodes(robots,resources,costs,minutes)
-        if resources_[3]>resources__[3]:
-            return resources_
-        else:
-            return resources__
+def next_obsidian(robots,resources,costs):
+    if robots[0]>0 and robots[1]>0:
+        return max([(rc-c)//r for r,c,rc in zip(robots,costs[2],resources) if c>0])
+    else:
+        return 0
 
-print(get_geodes(robots,resources,costs,0))
+def next_geode(robots,resources,costs):
+    if robots[0]>0 and robots[2]>0:
+        return max([(rc-c)//r for r,c,rc in zip(robots,costs[3],resources) if c>0])
+    else:
+        return 0
+
+def get_geodes(robots,resources,costs,minutes,maxgeodes):
+    print(minutes)
+    # print(robots)
+    print(resources[3])
+    # print(next_obsidian(robots,resources,costs))
+    if robots[2]==0 and next_obsidian(robots,resources,costs)+minutes>24:
+        return -1
+    if robots[3]==0 and next_geode(robots,resources,costs)+minutes>24:
+        return -1
+    if (robots[3]+1)*(24-minutes)+resources[3]<=maxgeodes: # and next_geode(robots,resources,costs)+minutes>24:
+        #assert False
+        return -1
+    if minutes==24:
+        return resources[3]
+    
+    purchases = get_purchases(resources,costs)
+    # print(purchases)
+    minutes+=1
+    resources=[rc+r for rc,r in zip(resources,robots)]
+    # maxgeodes=0
+    for i in range(len(purchases)-1,-1,-1):
+        if purchases[i]>1:
+            print('Purchase',i)
+            newrobots=robots.copy()
+            newrobots[i]+=1
+            newresources=[r-c for r,c in zip(resources,costs[i])]
+            maxgeodes=max(maxgeodes,get_geodes(newrobots,newresources,costs,minutes,maxgeodes))
+    return max(maxgeodes,get_geodes(robots,resources,costs,minutes,maxgeodes))
+
+print(get_geodes(robots,resources,costs,0,3))
